687fdf6079c56274f486c9c1357c4401d8c609ac
didula08 <didulanethmina2000@gmail.com>
2025-06-22 12:38:56 +0530

    create update function

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteProduct, getProducts,saveProduct } from '../controller/productController.js';
+import { deleteProduct, getProducts,saveProduct, updateProduct } from '../controller/productController.js';
 
 const productRouter=express.Router();
 
 productRouter.get("/",getProducts);
 productRouter.post("/",saveProduct);
 productRouter.delete("/:productId",deleteProduct)
+productRouter.put("/:productId",updateProduct)
 
 export default productRouter;
