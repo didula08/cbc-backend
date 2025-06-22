18985542bc9dd247955a71e35377144168fe91f7
didula08 <didulanethmina2000@gmail.com>
2025-06-22 14:52:54 +0530

    getproduct by id create

--- controller/productController.js
+++ controller/productController.js
@@ -95,4 +95,40 @@ export async function updateProduct(req,res) {
     }
 }
     
+export async function getProductById(req,res){
+    const productId = req.params.productId
+    
+    try{
 
+        const product = await Product.findOne(
+            {productId : productId}
+        )
+
+        if(product == null){
+            res.status(404).json({
+                message : "Product not found"
+            })
+            return
+        }
+        if(product.isAvailable){
+            res.json(product)
+        }else{
+            if(!isAdmin(req)){
+                res.status(404).json({
+                    message : "Product not found"
+                })
+                return
+            }else{
+                res.json(product)
+            }
+        }
+
+    }catch(err){
+        res.status(500).json({
+            message : "Internal server error",
+            error : err
+        })
+    }
+
+
+}
